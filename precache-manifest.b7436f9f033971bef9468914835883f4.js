f24fb6a843780aedbb755e358dd58870c7df9bd9
Amit Srivastava <amit.srivastava4all@gmail.com>
2020-09-05 09:04:09 +0530

    Add files via upload

---
+++ precache-manifest.b7436f9f033971bef9468914835883f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "634e1221bfe2b5df085042a533a541f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98168c2f9f337cc8f6b4",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b0b578dee9c7975d289f",
+    "url": "/static/js/2.34e9d54d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.34e9d54d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98168c2f9f337cc8f6b4",
+    "url": "/static/js/main.40c7f5c7.chunk.js"
+  },
+  {
+    "revision": "705c69162250a325d3b6",
+    "url": "/static/js/runtime-main.f2e4f4fd.js"
+  }
+]);
